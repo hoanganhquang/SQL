cdf89fc3c1bc6ac7fd408406e8311282764f7994
hoanganhquang <anhquang976@gmail.com>
2021-08-09 21:16:53 +0700

    updated

--- test.sql
+++ test.sql
@@ -1,7 +1,14 @@
--- create table tweets(
--- content varchar(100),
--- username varchar(10),
--- time_create timestamp default now()
--- );
+-- 0
+-- 1
+-- 1
 
-select * from tweets;
+select * from books where released_year < 1980;
+select * from books where author='Eggers' or author='Chabon'; select * from books where author in ('Eggers', 'Chabon');
+select * from books where author='Lahiri' and released_year > 2000;
+select * from books where pages between 100 and 200;
+select * from books where author_lname like 'c%' or author_lname like 's%';
+select *, case
+when title like '%stories%' then 'Short stories'
+when title like 'just kids and a heartbreaking work' then 'Memoir'
+else 'Novel'
+end as 'Type';
